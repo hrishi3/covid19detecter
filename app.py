5e3721bce65817302dc1c8ca6049f7c4de4107c4
hrishi3 <hrishikeshshinde6@gmail.com>
2020-05-18 12:17:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Use pickle to load in the pre-trained model
-with open(f'model/covid19detector1.pkl', 'rb') as f:
+with open(f'model/covid19detector (1).pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Initialise the Flask app
